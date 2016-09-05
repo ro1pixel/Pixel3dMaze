627080b3fa8154602043b354db3b28b62e9ca29e
Roysha <Roysha@Roysh-PC>
2016-09-05 23:43:24 +0300

    Need to finish Decomp.

--- my algorithms/src/boot/Exe3Run.java
+++ my algorithms/src/boot/Exe3Run.java
@@ -1,5 +1,12 @@
 package boot;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import algorithms.mazeGenerators.GrowingTreeGenerator;
 import algorithms.mazeGenerators.LastPositionChooser;
 import algorithms.mazeGenerators.Maze3d;
@@ -7,6 +14,8 @@ import algorithms.mazeGenerators.Maze3dGenerator;
 import algorithms.mazeGenerators.Position;
 import algorithms.mazeGenerators.RandomPositionChooser;
 import algorithms.mazeGenerators.SimpleMaze3dGenerator;
+import io.MyCompressorOutputStream;
+import io.MyDecompressorInputStream;
 
 public class Exe3Run {
 
@@ -21,8 +30,75 @@ public class Exe3Run {
 		//System.out.println("Algorithm Runtime: " +mg.measureAlgorithmTime(4,12,24));
 		
 		// generate another 3d maze
-		Maze3d maze=mg.generate(4,600,9);
-		
-		maze.toByteArray();
+		Maze3d maze=mg.generate(3,30,30);	
+	
+	//print maze
+	int[][][] array2 = maze.getArray3d();
+	int numberOfFloors = 0;
+	for(int[][] arr3 : array2) {
+		System.out.println("\nFloor:" + numberOfFloors);
+		for(int[] arr2 : arr3) {
+			System.out.println();
+			for(int val:arr2) {
+				System.out.print(val+ " ");
+				
+			}
+		}
+		numberOfFloors++;
 	}
+	
+	try {
+		OutputStream out=new MyCompressorOutputStream(
+				new FileOutputStream("1.maz"));
+				try {
+					out.write(maze.toByteArray());
+					out.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				finally {
+					try {
+						out.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	try {
+		InputStream in=new MyDecompressorInputStream(
+				new FileInputStream("1.maz"));
+		byte b[]=new byte[maze.toByteArray().length];
+		try {
+			in.read(b);
+			//Maze3d loaded=new Maze3d(b);
+			//System.out.println(loaded.equals(maze));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+
+	
+	
+	
+}
+		
 }
--- my algorithms/src/io/MyDecompressorInputStream.java
+++ my algorithms/src/io/MyDecompressorInputStream.java
@@ -1,28 +1,44 @@
 package io;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class MyDecompressorInputStream extends InputStream {
 	
 	InputStream in;
 
-	public MyDecompressorInputStream() {
+	public MyDecompressorInputStream(InputStream in) {
 		super();
-		in = new InputStream() {
-			
-			@Override
-			public int read() throws IOException {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-		};
+		this.in = in;
 	}
 	
 	@Override
 	public int read() throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+		return in.read();
+	}
+	
+	public int read(byte[] array) throws IOException {
+		int i=0;
+		@SuppressWarnings("resource")
+		String content = new Scanner(new File("1.maz")).useDelimiter("\\Z").next();
+		String[] split = content.split(",");
+		
+		for(;i<9;i++) {
+			//array[i] = split[i].getBytes();
+		}
+		
+		while(i<array.length) {
+			int count = in.read();
+			byte b = (byte) in.read();
+			
+			for(int j=0;j<count;j++) {
+				array[i++] = b;
+			}
+		}
+		
+		return array.length;
 	}
 
 }
