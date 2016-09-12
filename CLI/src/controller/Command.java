7bb917890d47e615ed74bc86b3fbbf205ab715c1
Roysha <Roysha@Roysh-PC>
2016-09-12 22:36:13 +0300

    Javadoc

--- CLI/src/controller/Command.java
+++ CLI/src/controller/Command.java
@@ -1,5 +1,14 @@
 package controller;
 
+/**
+ * The Interface Command.
+ */
 public interface Command {
+	
+	/**
+	 * Do command.
+	 *
+	 * @param args the args
+	 */
 	void doCommand(String[] args);
 }
--- CLI/src/view/View.java
+++ CLI/src/view/View.java
@@ -11,15 +11,79 @@ import algorithms.search.State;
 import controller.Command;
 import controller.Controller;
 
+/**
+ * The Interface View.
+ */
 public interface View {
+	
+	/**
+	 * Start.
+	 */
 	void start();
+	
+	/**
+	 * Notify maze is ready.
+	 *
+	 * @param name the name
+	 */
 	void notifyMazeIsReady(String name);
+	
+	/**
+	 * Display maze.
+	 *
+	 * @param maze the maze
+	 */
 	void displayMaze(Maze3d maze);
+	
+	/**
+	 * Display cross section.
+	 *
+	 * @param maze the maze
+	 * @param z the z
+	 * @param y the y
+	 * @param x the x
+	 */
 	void displayCrossSection(Maze3d maze,int z,int y,int x);
+	
+	/**
+	 * Sets the commands.
+	 *
+	 * @param commands the commands
+	 */
 	void setCommands(HashMap<String, Command> commands); 
+	
+	/**
+	 * Display solution.
+	 *
+	 * @param solution the solution
+	 */
 	void displaySolution(Solution<Position> solution);
+	
+	/**
+	 * Notify solution is ready.
+	 *
+	 * @param name the name
+	 */
 	void notifySolutionIsReady(String name);
+	
+	/**
+	 * Display files.
+	 *
+	 * @param files the files
+	 */
 	void displayFiles(File[] files);
+	
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller the new controller
+	 */
 	void setController(Controller controller);
+	
+	/**
+	 * Prints the to screen.
+	 *
+	 * @param string the string
+	 */
 	void printToScreen(String string);
 }
